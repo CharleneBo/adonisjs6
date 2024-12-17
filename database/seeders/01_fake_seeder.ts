f4c47a6f3eed21ebf438a1360d0a10686dd1abb4
MarionQH <marion.huet49@gmail.com>
2024-12-17 13:42:43 +0100

    fix: fake_seeder

--- database/seeders/01_fake_seeder.ts
+++ database/seeders/01_fake_seeder.ts
@@ -1,3 +1,4 @@
+import { movies } from '#database/data/movie'
 import { CineastFactory } from '#database/factories/cineast_factory'
 import { MovieFactory } from '#database/factories/movie_factory'
 import { UserFactory } from '#database/factories/user_factory'
@@ -14,24 +15,25 @@ export default class extends BaseSeeder {
     await this.#createMovies()
   }
 
-  async #createMovies(){
-      let index = 0
-      await MovieFactory.tap((row, { faker }) => {
-        const movie = movies[index]
-        const released = DateTime.now().set({ year: movie.releasedYear})
-        row.title = movie.title
-        row.releasedAt = DateTime.fromJSDate(
-          faker.date.between({
-            from: released.startOf('year').toJSDate(),
-            to: released.endOf('year').toJSDate(),
-          })
-        )
-        index ++
-      }).createMany(movies.length)
-      await MovieFactory.createMany(3)
-      await MovieFactory.merge({
+  async #createMovies() {
+    let index = 0
+    await MovieFactory.tap((row, { faker }) => {
+      const movie = movies[index]
+      const released = DateTime.now().set({ year: movie.releaseYear })
+      row.title = movie.title
+      row.releasedAt = DateTime.fromJSDate(
+        faker.date.between({
+          from: released.startOf('year').toJSDate(),
+          to: released.endOf('year').toJSDate(),
+        })
+      )
+      index++
+    }).createMany(movies.length)
+
+    await MovieFactory.createMany(3)
+    await MovieFactory.merge({
       statusId: MovieStatuses.RELEASED,
       releasedAt: DateTime.now().minus({ month: 1 }),
     }).createMany(2)
-    }
-  
+  }
+}
