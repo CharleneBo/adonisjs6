bf4332e3bd3f428c1bafc030e65a5c1d8a802ff5
MarionQH <marion.huet49@gmail.com>
2025-01-02 15:32:03 +0100

    feat: ajout du bouton add to watchlist

--- app/services/movie_service.ts
+++ app/services/movie_service.ts
@@ -1,4 +1,5 @@
 import Movie from '#models/movie'
+import User from '#models/user'
 import { movieFilterValidator } from '#validators/movie'
 import { Infer } from '@vinejs/vine/types'
 
@@ -17,7 +18,11 @@ export class MovieService {
     { id: 'writer_asc', text: 'Writer Name (asc)', field: 'cineasts.last_name', dir: 'asc' },
     { id: 'writer_desc', text: 'Writer Name (desc)', field: 'cineasts.last_name', dir: 'desc' },
   ]
-  static getFiltered(page: number = 1, filters: Infer<typeof movieFilterValidator>) {
+  static getFiltered(
+    page: number = 1,
+    filters: Infer<typeof movieFilterValidator>,
+    user: User | undefined = undefined
+  ) {
     const sort =
       this.sortOptions.find((option) => option.id === filters.sort) || this.sortOptions[0]
     return Movie.query()
@@ -26,6 +31,9 @@ export class MovieService {
       .if(['writer_asc', 'writer_desc'].includes(sort.id), (query) => {
         query.join('cineasts', 'cineasts.id', 'writer_id').select('movies.*')
       })
+      .if(user, (query) =>
+        query.preload('watchlist', (watchlist) => watchlist.where('userId', user!.id))
+      )
       .preload('director')
       .preload('writer')
       .preload('status')
